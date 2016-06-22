c345e7e1493931a764391608732ddece0dccc3e3
ivahaev <ivahaev@gmail.com>
2016-06-23 00:03:25 +0500

    Auto versioning

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	log "github.com/Sirupsen/logrus"
@@ -10,16 +11,27 @@ import (
 	"github.com/getblank/blank-queue/queue"
 )
 
+var (
+	buildTime string
+	version   string
+	gitHash   string
+)
+
 func main() {
 	log.SetLevel(log.DebugLevel)
 	var srAddress *string
 	var port *string
 	var dbFile *string
+	var verFlag *bool
 	rootCmd := &cobra.Command{
 		Use:   "blank-queue",
 		Short: "Queue microservice for Blank platform",
 		Long:  "The next generation of web applications. This is the queue subsystem.",
 		Run: func(cmd *cobra.Command, args []string) {
+			if *verFlag {
+				printVersion()
+				return
+			}
 			log.Info("blank-queue started")
 			go queue.Init(*dbFile)
 			intranet.Init(*srAddress, *port)
@@ -29,9 +41,20 @@ func main() {
 	srAddress = rootCmd.PersistentFlags().StringP("service-registry", "s", "ws://localhost:1234", "Service registry uri")
 	port = rootCmd.PersistentFlags().StringP("port", "p", "8083", "TCP port to listen")
 	dbFile = rootCmd.PersistentFlags().StringP("db", "d", "queue.db", "Queue database filename")
+	verFlag = rootCmd.PersistentFlags().BoolP("version", "v", false, "Prints version and exit")
 
 	if err := rootCmd.Execute(); err != nil {
 		println(err.Error())
 		os.Exit(-1)
 	}
+}
+
+func printVersion() {
+	fmt.Printf("Build time:  		%s\n", buildTime)
+	fmt.Printf("Commit hash: 		%s\n", gitHash)
+	if version == "" {
+		fmt.Println("This build has not version.")
+		return
+	}
+	fmt.Printf("Version:     		%s\n", version)
 }
