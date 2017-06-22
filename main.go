b72fc209add4a6d9e50f097730028fae5b8a3154
Evgeniy Ivakha <ivahaev@gmail.com>
2017-06-22 12:53:31 +0500

    Parsing BLANK_SERVICE_REGISTRY_URI and BLANK_SERVICE_REGISTRY_PORT env vars

--- main.go
+++ main.go
@@ -50,6 +50,14 @@ func main() {
 		return
 	}
 
+	if sr := os.Getenv("BLANK_SERVICE_REGISTRY_URI"); len(sr) > 0 {
+		srAddress = &sr
+	}
+	if srPort := os.Getenv("BLANK_SERVICE_REGISTRY_PORT"); len(srPort) > 0 {
+		addr := "ws://localhost:" + srPort
+		srAddress = &addr
+	}
+
 	log.Info("blank-queue started")
 	go queue.Init(*qdbFile)
 	go lists.Init(*ldbFile)
